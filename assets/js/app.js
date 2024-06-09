7c6152686b6550320eaa1057fc187f18801ca806
4Scandi <116696944+4Scandi@users.noreply.github.com>
2024-06-09 10:23:54 +0700

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-// Constants Variables
-const footerYear = document.querySelector('footer span');
-
-const getCurrentYear = _ => new Date().getFullYear();
-const footerSetYear = _ => footerYear.textContent = getCurrentYear();
-
-footerSetYear();
